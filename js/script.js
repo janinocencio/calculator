590a0a015f4e50ed5048eba637f7cc563b02b034
janinocencio <janinocencio@gmail.com>
2023-12-27 11:41:19 +1100

    Add query selectors for buttons

--- js/script.js
+++ js/script.js
@@ -20,3 +20,40 @@ function operate(a,operator,b) {
     if (operator === "*") return multiply(a,b);
     if (operator === "/") return divide(a,b);
 };
+
+
+const btnClear = document.querySelector('#clear');
+
+const btnDelete = document.querySelector('#delete');
+
+const btnSeven = document.querySelector('#seven');
+
+const btnEight = document.querySelector('#eight');
+
+const btnSeven = document.querySelector('#nine');
+
+const btnDivide = document.querySelector('#divide');
+
+const btnFour = document.querySelector('#four');
+
+const btnFive = document.querySelector('#five');
+
+const btnSix = document.querySelector('#six');
+
+const btnMultiply = document.querySelector('#multiply');
+
+const btnOne = document.querySelector('#one');
+
+const btnTwo = document.querySelector('#two');
+
+const btnThree = document.querySelector('#three');
+
+const btnSubtract = document.querySelector('#subtract');
+
+const btnDot = document.querySelector('#dot');
+
+const btnZero = document.querySelector('#zero');
+
+const btnEqual = document.querySelector('#equal');
+
+const btnAdd = document.querySelector('#add');
